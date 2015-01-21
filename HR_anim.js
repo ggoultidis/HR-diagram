d0c701269e998ca5c7c72c0525e0a32005e8b4c5
George Goultidis <george.goultidis@gmail.com>
2015-01-21 12:54:38 +0100

    attempt to fix HR animation

--- HR_anim.js
+++ HR_anim.js
@@ -1,11 +1,27 @@
+// var binary1_sel = true;
+// var binary2_sel = false;
+// var binary3_sel = false;
+// var binary4_sel = false;
+// var binary5_sel = false;
+// var binary6_sel = false;
+
+
+// handle on click event
+d3.select("#binary_menu")
+  .on("change", function() {
+    console.log(d3.select(this).property("value"));
+    // updateLegend(newData);
+});
+
+
+
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom; 
 
-// var globalData;
 
 
-// var axis_format = d3.format("04d");
+var axis_format = d3.format("01d");
 
 var x = d3.scale.log()
   .domain([3.5,6])
@@ -21,7 +37,6 @@ var xAxis = d3.svg.axis()
     .orient("bottom")
     // .tickFormat(axis_format)
     // .tickValues([1000,3000,6000,10000,20000,40000]);
-    // .tickValues([1000, 2000,3000,5000,10000,15000,20000,30000,50000]);
 
 
 var yAxis = d3.svg.axis()
@@ -32,10 +47,10 @@ var yAxis = d3.svg.axis()
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .attr("id", "graph1")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// console.log(data.points);
 
 // Gives domain between min and max >> .extent <<
 // x.domain(d3.extent(data.points, function(d) { return parseInt(d[0]); }));
@@ -69,52 +84,101 @@ svg.append("g")
 var data_sets = ["HR_d_6.json", "HR_a_6.json"];
 
 for(var i = 0; i < data_sets.length; i++){
+  console.log(i);
 
   // callback function
   d3.json(data_sets[i], function(error, data){
 
-    var stars = svg.selectAll("dot")
+    var star = svg.selectAll("dot")
         .data([data.points[0]])
-    .enter().append("circle")
-      .attr("r", function(d) { return d[5]; })
-      .attr("cx", function(d) { return x(d[3]); })
-      .attr("cy", function(d) { return y(d[4]); })
-      .style("fill", "red");
+      .enter().append("circle")
+        .attr("r", function(d) { return d[6]; })
+        .attr("cx", function(d) { return x(d[4]); })
+        .attr("cy", function(d) { return y(d[5]); })
+        .style("fill", "red");
 
-    // console.log("here?");
+
+    var total_duration = 10000,
+      steps = data.points.length,
+      time_step = total_duration / steps;
     
-    var duration = 5;
-    var delay = 0;
+    var duration = 5,
+     delay = 0;
+
+    console.log(time_step);
+
+
+
+    var line = d3.svg.line()
+      .interpolate("linear")
+      .x(function(d) {return x(d[4]);})
+      .y(function(d) {return y(d[5]);})
+
+    var path = svg.append("path")
+      .attr("d", line(data.points))
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", "2")
+      .attr("fill", "none");
+
+    var totalLength = path.node().getTotalLength();
+
+    path
+      .attr("stroke-dasharray", totalLength + " " + totalLength)
+      .attr("stroke-dashoffset", totalLength)
+      .transition()
+        .duration(total_duration)
+        .ease("linear")
+        .attr("stroke-dashoffset", 0);
+
+
+
+
+
+    
     
     function update(model_num){
-      stars.data([data.points[model_num]]);
-      if (data.points.length - 1 > (model_num % 3300)){
+      star.data([data.points[model_num]]);
+      if (steps - 1 > (model_num % steps)){
 
-        var tr = stars.transition()
-          .duration(duration)
-          .attr("r", function(d) { return d[5]; })
-          .attr("cx", function(d) { return x(d[3]); })
-          .attr("cy", function(d) { return y(d[4]); })
+        var tr = star.transition()
+          .duration(time_step)
+          .attr("r", function(d) { return d[6]; })
+          .attr("cx", function(d) { return x(d[4]); })
+          .attr("cy", function(d) { return y(d[5]); })
           .style("fill", "blue");
 
-        // console.log(model_num)
+        /*
+        if (data.points[0][0] == "a"){
+          var path = "pathA";
+        }
+        else if(data.points[0][0] == "d"){
+          var path = "pathD";
+        }
+
+        var delete_line = d3.selectAll(path).remove();
+
+        var line_path =[];
+
+        for (var j = 0; j < model_num; j++){
+          line_path.push({"X" : data.points[j][4], "Y" : data.points[j][5]});  
+        }
+        
+        var draw_line = d3.svg.line()
+          .x(function(d){return x(d.X);})
+          .y(function(d){return y(d.Y);})
+          .interpolate("linear");
+
+        var line_graph = svg.append("path")
+          .attr("d", draw_line(line_path))
+          .attr("stroke", "blue")
+          .attr("stroke-width", 1)
+          .attr("fill", "none")
+          // .attr("class", path);
+        */
       }
       setTimeout(function(){update((model_num + 1) % 3300);}, duration + delay);
     }
-    
     update(0);
-
-    // console.log("now?");
-
-  /*
-    svg.selectAll("dot")
-        .data([data.points[10]])
-    .enter().append("circle")
-      .attr("r", function(d) { return d[5]; })
-      .attr("cx", function(d) { return x(d[3]); })
-      .attr("cy", function(d) { return y(d[4]); })
-      .style("fill", "red");
-  */
 
 
     // gets the base 10 log of the effective Temperature and uses the peak of Planck's
@@ -122,12 +186,8 @@ for(var i = 0; i < data_sets.length; i++){
     function getColor (log_T){
       return log_T;
     }
+    function hello(){
+      console.log("hello")
+    }
   });
-}
-
-
-var div_list = ["#table1","#table2"];
-
-for(var i = 0; i< div_list.length; i++){
-  select(div_list[i])
 }
